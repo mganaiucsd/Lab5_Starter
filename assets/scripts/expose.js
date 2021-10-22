1336def5b29fcb5ad575dabc3070f9d095ad4ff9
Milan Ganai <milanganai@gmail.com>
2021-10-22 12:26:53 -0700

    Add sound with button clicked

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -2,35 +2,34 @@
 
 window.addEventListener('DOMContentLoaded', init);
 
-const selectSound = document.getElementById('horn-select');
-selectSound.addEventListener('change', (event) => {
-  const audio = document.querySelector('.hidden');
-  audio.src = `assets/audio/${event.target.value}.mp3`;
-  const image = document.querySelector('section > img');
-  image.src = `assets/images/${event.target.value}.svg`;
-  console.log(event.target.value);
-  console.log(selectSound);
-});
-
-const inputVolume = document.querySelector("[type='range']");
-inputVolume.addEventListener('input', (event) => {
-  const audio = document.querySelector('.hidden');
-  audio.volume = event.target.value/100;
-  const image = document.querySelector('div > img');
-  let level = 0;
-  if (event.target.value < 33) {
-    level = 1;
-  } else if (event.target.value < 67) {
-    level = 2;
-  } else {
-    level = 3;
-  }
-  image.src = `assets/icons/volume-level-${level}.svg`;
-});
-
-
 function init() {
-  // assuming this is the right spot to put this:
-
-
+  const selectSound = document.getElementById('horn-select');
+  selectSound.addEventListener('change', (event) => {
+    const audio = document.querySelector('.hidden');
+    audio.src = `assets/audio/${event.target.value}.mp3`;
+    const image = document.querySelector('section > img');
+    image.src = `assets/images/${event.target.value}.svg`;
+  });
+  
+  const inputVolume = document.querySelector("[type='range']");
+  inputVolume.addEventListener('input', (event) => {
+    const audio = document.querySelector('.hidden');
+    audio.volume = event.target.value/100;
+    const image = document.querySelector('div > img');
+    let level = 0;
+    if (event.target.value < 33) {
+      level = 1;
+    } else if (event.target.value < 67) {
+      level = 2;
+    } else {
+      level = 3;
+    }
+    image.src = `assets/icons/volume-level-${level}.svg`;
+  });    
+  
+  const playSound = document.querySelector('button');
+  playSound.addEventListener('click', event => {
+    const audio = document.querySelector('.hidden');
+    audio.play();
+  });
 }
