6c35d1878d84b2284339acee8871d6451da3eb37
Milan Ganai <milanganai@gmail.com>
2021-10-23 13:16:59 -0700

    Update speaking

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -3,5 +3,41 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
-  // TODO
+  var synth = window.speechSynthesis;
+  var inputTxt = document.getElementById('text-to-speak');
+  var voiceSelect = document.getElementById('voice-select');
+  var voices = [];
+
+  function populateVoiceList() {
+    voices = synth.getVoices();
+  
+    for(var i = 0; i < voices.length ; i++) {
+      var option = document.createElement('option');
+      option.textContent = voices[i].name + ' (' + voices[i].lang + ')';
+  
+      if(voices[i].default) {
+        option.textContent += ' -- DEFAULT';
+      }
+  
+      option.setAttribute('data-lang', voices[i].lang);
+      option.setAttribute('data-name', voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+  populateVoiceList();
+  const speak = document.querySelector('button');
+  speak.addEventListener('click', (event) => {
+    event.preventDefault();
+    var utterThis = new SpeechSynthesisUtterance(inputTxt.value);
+    var selectedOption = voiceSelect.selectedOptions[0].getAttribute('data-name');
+    for(var i = 0; i < voices.length ; i++) {
+      if(voices[i].name === selectedOption) {
+        utterThis.voice = voices[i];
+        break;
+      }
+    }
+    synth.speak(utterThis);
+  });  
+  
 }
+
